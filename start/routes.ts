459e76e794f7a60df4c4bed4394c887e93a63d19
Jardson Alan <jardsonalan132@gmail.com>
2025-05-30 22:38:22 -0300

    feat: adicionando as rotas de equipes

--- start/routes.ts
+++ start/routes.ts
@@ -9,8 +9,9 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.get('/equipes', '#controllers/equipes_controller.index')
+router.get('/equipes/:id', '#controllers/equipes_controller.show')
+router.post('/equipes', '#controllers/equipes_controller.store')
+router.put('/equipes/:id', '#controllers/equipes_controller.update')
+router.patch('/equipes/:id', '#controllers/equipes_controller.update')
+router.delete('/equipes/:id', '#controllers/equipes_controller.destroy')
