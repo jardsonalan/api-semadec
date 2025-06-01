5e2da72ab9ae51776b715fd6841586a7758322a0
Jardson Alan <jardsonalan132@gmail.com>
2025-06-01 15:39:55 -0300

    feat: atualizando métodos da controller de equipes para pré-carregar os dados do relacionamento com atletas

--- app/controllers/equipes_controller.ts
+++ app/controllers/equipes_controller.ts
@@ -3,23 +3,33 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class EquipesController {
   public async index({}: HttpContext) {
-    return await Equipe.all()
+    return await Equipe.query().preload('atletas')
   }
 
   public async show({ response, params }: HttpContext) {
-    try {
-      return await Equipe.find(params.id)
-    } catch {
-      return await response.json({
+    const equipe = await Equipe.query().preload('atletas').where('id', params.id)
+
+    if (equipe.length === 0) {
+      return await response.status(404).json({
         errors: {
           default: 'Equipe não encontrada.',
         },
       })
     }
+
+    return await equipe
   }
 
   public async store({ request }: HttpContext) {
-    return await Equipe.create(request.only(['curso', 'nome']))
+    const { curso, nome, atletas } = await request.only(['curso', 'nome', 'atletas'])
+
+    const equipe = await Equipe.create({ curso, nome })
+
+    if (atletas && atletas.length > 0) {
+      await equipe.related('atletas').createMany(atletas)
+    }
+
+    return await equipe.id
   }
 
   public async update({ response, request, params }: HttpContext) {
